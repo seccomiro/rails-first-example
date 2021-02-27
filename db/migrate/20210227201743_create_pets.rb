2d79a805e02a11fb1417c92af7857cfd96bb2e6f
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-02-27 17:21:22 -0300

    Create Pet scaffold

---
+++ db/migrate/20210227201743_create_pets.rb
@@ -0,0 +1,10 @@
+class CreatePets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.references :person, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
